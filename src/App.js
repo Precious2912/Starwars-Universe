2c1638f4699ef706bfc0a61dd135cf9546ffae96
Precious2912 <preciousovat@gmail.com>
2022-11-18 14:51:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      characters: [],
+      searchField: "",
+    };
+  }
+
+  onSearchField = (e) => {
+    const searchField = e.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  componentDidMount() {
+    fetch("https://akabab.github.io/starwars-api/api/all.json")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return {
+              characters: users,
+            };
+          }
+        )
+      );
+  }
+
+  render() {
+    const { characters, searchField } = this.state;
+    const { onSearchField } = this;
+
+    const filteredCharacters = characters.filter((character) => {
+      return character.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search character"
+          onChange={onSearchField}
+        />
+        {filteredCharacters.map((character) => {
+          return (
+            <div key={character.id}>
+              <h1>{character.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
